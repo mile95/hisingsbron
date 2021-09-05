b7a760fbe822e8d2d844911ca84cfabb0e1e27b7
Fredrik Mile <fredrik.mile@gmail.com>
2021-09-05 16:49:14 +0200

    Init commit

---
+++ hissing-bridge/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hissingsbron
+        </h1>
+      </header>
+      <body className="App-body">
+        <div>
+          <h1> Status </h1>
+          <h2> Stängd </h2>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
