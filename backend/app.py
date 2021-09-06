a2eb86ea219e951df4524c113b85e5c605337feb
Fredrik Mile <fredrik.mile@gmail.com>
2021-09-06 07:25:11 +0200

    Add current-state and history endpoints in backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,22 @@
 from typing import Optional
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/current-status")
+def get_current_status():
+    """
+    TODO: Get latest entry from database
+    """
+    return {"Status": "Open"}
+
+@app.get("/history")
+def get_history(from_date: datetime, to_date: datetime):
+    # Example 2021-09-05T18:19:04Z
+    # Example 2021-09-05T20:19:04+02:00
+    """
+    TODO: Get entries between from_date and to_date from database
+    """
+    return {"fakedata":"fakedata"}
