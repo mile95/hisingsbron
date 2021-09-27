1cbd07f5ff2a1d4d1c2f34fc1fe9419163a79f15
Fredrik Mile <fredrik.mile@gmail.com>
2021-09-27 08:30:45 +0200

    Add script for building and pushing images to ecr

---
+++ build-and-push-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ACCOUNT_ID=$1
+
+# Build backend image
+docker build -t $ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/hissingsbridgerepository:backend backend/
+
+# Build frontend image
+docker build -t $ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/hissingsbridgerepository:frontend frontend/
+
+# Log into AWS ECR
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com
+
+# Push images to ECR
+docker push $ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/hissingsbridgerepository:backend
+docker push $ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/hissingsbridgerepository:frontend
